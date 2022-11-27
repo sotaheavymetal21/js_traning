2fad8fbe6d56d720c0bc6932adba5d216abed845
Apple <apple@Sotanomakku.local>
2022-11-27 21:19:20 +0900

    map, filter

--- src/index.js
+++ src/index.js
@@ -69,28 +69,101 @@
 // function func1(str) {
 //     return str;
 // }
-const func1 = function (str) {
-    return str;
-}
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//     return str;
+// }
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => {
-    return str;
-}
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => {
+//     return str;
+// }
+// console.log(func2("func2です"));
 
-// returnを書かなくても良い
-const func3 = (str) => str;
-console.log(func3("func3です"))
+// // returnを書かなくても良い
+// const func3 = (str) => str;
+// console.log(func3("func3です"))
 
-const func4 = (num1, num2) => {
-    return num1 + num2;
-};
-console.log(func4(10, 20));
+// const func4 = (num1, num2) => {
+//     return num1 + num2;
+// };
+// console.log(func4(10, 20));
 
-const func5 = (num1, num2) => num1 + num2;
-console.log(func5(10, 20));
+// const func5 = (num1, num2) => num1 + num2;
+// console.log(func5(10, 20));
+
+/**
+ * 分割代入
+ */
+
+// const myProfile = {
+//     name: "sota",
+//     age: 24,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile
+// const message2 = `名前は${name}です。年齢は${age}歳です。`
+// console.log(message2);
+
+// const myProfile = ["壮太", 24];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message3);
+
+// const [ name, age ] = myProfile;a
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
 
 
+/**
+ * デフォルト値、引数など
+ */
 
+// const sayHello = (name = "そうた") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+/**
+ * スプレッド構文
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1, num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+const nameArr = ["Tatsuya", "Kayo", "Sota"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr)
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "Sota") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
